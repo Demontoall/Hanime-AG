762bc78c22ae5ca78b9bb4bc7501cd86531426b6
Demontoall <163480843+Demontoall@users.noreply.github.com>
2026-07-01 04:53:52 +0100

    Implement sidebar toggle functionality
    Update on side bar

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const menu = document.getElementById("menu");
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
 
+menu.onclick = () => {
+  sidebar.classList.toggle("active");
+  overlay.classList.toggle("show");
+};
+
+overlay.onclick = () => {
+  sidebar.classList.remove("active");
+  overlay.classList.remove("show");
+};
